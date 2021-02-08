077feb07be0c996af70ded3618dfacb5301f39b7
Mohammed Esafi <mesafi@student.1337.ma>
2021-02-08 21:36:20 +0100

    add navLinks, breakpoints and fontSize configurations

--- src/config.js
+++ src/config.js
@@ -1,1 +1,37 @@
-module.exports = {};
+module.exports = {
+	navLinks: [
+		{
+			name: 'How it works',
+			url: '/#usage'
+		},
+		{
+			name: 'Who we are',
+			url: '/#about'
+		},
+		{
+			name: 'What we do',
+			url: '/#services'
+		},
+		{
+			name: 'Contact us',
+			url: '/#contact'
+		}
+	],
+
+	breakpoints: {
+		desktop: 1000 / 16,
+		tablet: 768 / 16,
+		phone: 480 / 16
+	},
+
+	fontSize: {
+		xxs: 12 / 16,
+		xs: 13 / 16,
+		sm: 14 / 16,
+		md: 16 / 16,
+		lg: 18 / 16,
+		xl: 20 / 16,
+		xxl: 22 / 16,
+		heading: 32 / 16
+	}
+};
