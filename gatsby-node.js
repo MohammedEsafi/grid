a4092e579d9de69cacced20c4d3c682c29e11a72
Mohammed Esafi <mesafi@student.1337.ma>
2021-02-08 21:34:23 +0100

    set up import aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,9 @@ exports.onCreateWebpackConfig = ({ actions }) => {
 				'@components': path.resolve(__dirname, 'src/components'),
 				'@styles': path.resolve(__dirname, 'src/styles'),
 				'@widgets': path.resolve(__dirname, 'src/widgets'),
-				'@fonts': path.resolve(__dirname, 'src/fonts')
+				'@fonts': path.resolve(__dirname, 'src/fonts'),
+				'@config': path.resolve(__dirname, 'src/config'),
+				'@utils': path.resolve(__dirname, 'src/utils')
 			}
 		}
 	});
