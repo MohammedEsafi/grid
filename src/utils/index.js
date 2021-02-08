a47e0e4aa6a9d3d6d4fdb7a6c410801b7bfdc49c
Mohammed Esafi <mesafi@student.1337.ma>
2021-02-08 21:42:03 +0100

    hex2rgba and clamping helpers -> utils

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+export const hex2rgba = (hex, alpha = 1) => {
+	const [r, g, b] = hex.match(/\w\w/g).map((x) => parseInt(x, 16));
+
+	return `rgba(${r},${g},${b},${alpha})`;
+};
+
+// Takes the viewport widths in em and the font sizes in rem
+export const clamping = (minWidth, maxWidth, minFontSize, maxFontSize) => {
+	const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth);
+	const intersection = -minWidth * slope + minFontSize;
+
+	return `clamp( ${minFontSize}rem, ${intersection}rem + ${slope * 100}vw, ${maxFontSize}rem )`;
+	return `max(${minFontSize}rem, min(${intersection}rem + ${slope * 100}vw, ${maxFontSize}rem))`;
+};
