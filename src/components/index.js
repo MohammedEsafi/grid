edf4c6d6dfac73c8ac7c8c1379a40b68c8f92b0d
Mohammed Esafi <mesafi@student.1337.ma>
2021-02-12 11:39:24 +0100

    exporting `Nav`, `Button`, `Brand` and `Grid`

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as Layout } from './Layout';
 export { default as Head } from './Head';
+export { default as Nav } from './Nav';
 export { default as Menu } from './Menu';
+export { default as Button } from './Button';
+export { default as Brand } from './Brand';
+export { default as Grid } from './Grid';
