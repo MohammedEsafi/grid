ac6196b6d85a2b9dc06e34c85043cb5aff00068b
Mohammed Esafi <mesafi@student.1337.ma>
2021-02-12 11:35:12 +0100

    Hero section (section #1)

--- src/widgets/Hero.js
+++ src/widgets/Hero.js
@@ -1,17 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Menu } from '@components';
+import { Nav, Grid } from '@components';
+import config from '@config';
+import { clamping } from '@utils';
+
+const { breakpoints, fontSize, mainPadding } = config;
 
 const Header = styled.header`
 	display: flex;
 	flex-direction: column;
 `;
 
+const Content = styled.section`
+	--padding: ${clamping(breakpoints.phone, breakpoints.desktop, mainPadding.min, mainPadding.max)};
+	display: flex;
+	flex-direction: column;
+	min-height: calc(100vh - var(--nav-height));
+`;
+
+const Heading = styled.h1`
+	font-size: ${clamping(
+		breakpoints.phone,
+		breakpoints.desktop,
+		fontSize.heading - 2,
+		fontSize.heading
+	)};
+	font-weight: 800;
+	text-align: center;
+	color: var(--black);
+	text-shadow: var(--white-text-shadow);
+	margin-bottom: 0;
+
+	& > .red {
+		color: var(--red-dark);
+		position: relative;
+
+		&:after {
+			content: '';
+			width: 100%;
+			height: 35%;
+			background-color: var(--green-light);
+			position: absolute;
+			left: 0;
+			bottom: 7%;
+			z-index: -1;
+			border-radius: var(--r-border-radius);
+		}
+	}
+`;
+
+const SubTitle = styled.h2`
+	font-size: ${clamping(
+		breakpoints.phone,
+		breakpoints.desktop,
+		fontSize.heading - 2.5,
+		fontSize.heading - 1
+	)};
+	font-weight: 800;
+	text-align: center;
+	color: var(--black);
+	text-shadow: var(--white-text-shadow);
+	margin: 0.5em 0 1.5em;
+`;
+
 const Hero = () => {
 	return (
-		<Header>
-			<Menu />
-		</Header>
+		<>
+			<Header>
+				<Nav />
+			</Header>
+			<Content id='usage'>
+				<Heading>
+					Combine <span className='red'>fine</span> images
+				</Heading>
+				<SubTitle>To represent a product</SubTitle>
+				<Grid />
+			</Content>
+		</>
 	);
 };
 
