6212b5eabf9ee11097b2c2aabb6f7f681992245a
Mohammed Esafi <hello@mesafi.com>
2021-03-07 09:01:42 +0100

    add bottom image to footer

--- src/widgets/Footer.js
+++ src/widgets/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import config from '@config';
 import { clamping } from '@utils';
 import { Social, Info, Contact } from '@components';
 import { media } from '@styles';
-
+import wave from '../images/wave.svg';
 const { breakpoints, fontSize } = config;
 
 const Wrapper = styled.footer`
@@ -17,6 +17,8 @@ const Wrapper = styled.footer`
 	padding-bottom: 70px;
 	grid-template-columns: repeat(2, 1fr);
 	grid-template-rows: repeat(2, auto);
+	max-width: 1366px;
+	margin: 0 auto;
 
 	${media.tablet`
 		grid-template-columns: 1fr;
@@ -44,24 +46,27 @@ const Link = styled.a`
 
 const Footer = () => {
 	return (
-		<Wrapper>
-			<div>
-				<Title>follow us</Title>
-				<Social />
-			</div>
-			<div>
-				<Title>email us</Title>
-				<Link href='mailto:hello@mesafi.com'>hello@mesafi.com</Link>
-			</div>
-			<div>
-				<Title>information</Title>
-				<Info />
-			</div>
-			<div>
-				<Title>keep in touch</Title>
-				<Contact />
-			</div>
-		</Wrapper>
+		<>
+			<Wrapper>
+				<div>
+					<Title>follow us</Title>
+					<Social />
+				</div>
+				<div>
+					<Title>email us</Title>
+					<Link href='mailto:hello@mesafi.com'>hello@mesafi.com</Link>
+				</div>
+				<div>
+					<Title>information</Title>
+					<Info />
+				</div>
+				<div>
+					<Title>keep in touch</Title>
+					<Contact />
+				</div>
+			</Wrapper>
+			<img src={wave} />
+		</>
 	);
 };
 
